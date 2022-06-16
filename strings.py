0e37080cc358fcb3882f51bcb29f5329b08ca719
ssp <swalehapeerzade@gmail.com>
2022-06-16 13:59:50 -0400

    commit

---
+++ strings.py
@@ -0,0 +1,20 @@
+"""
+
+String is a sequence of characters
+It contains a-z, 0-9, @
+It is in single or double quotes
+
+"""
+
+str1 = "This string is in double quotes"
+str2 = 'This string is in single quotes'
+str3 = "This is a 'big' school"
+str4 = "There is a \"giant\" wheel in the park"
+str5 = "This is a very long\
+string"
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
