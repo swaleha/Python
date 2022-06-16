33df14768631c0cc94db8ed1e6b931192e59d3ff
ssp <swalehapeerzade@gmail.com>
2022-06-16 17:28:09 -0400

    commit

--- variables_rules.py
+++ variables_rules.py
@@ -1,6 +1,11 @@
 import keyword
 print(keyword.kwlist)
 variable_name = 10
+cars = 5
+_cars = 2
+CARS = 3
+cars2 = 15
 print(variable_name)
 a = b = c = 20
 print(a, b, c)
+print(cars,_cars,CARS,cars2)
