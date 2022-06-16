fa2d97ed273e7bc0de5b9625019df882f4196228
ssp <swalehapeerzade@gmail.com>
2022-06-16 13:47:30 -0400

    commit

---
+++ boolean.py
@@ -0,0 +1,17 @@
+a = True
+b = False
+
+print("a =",a)
+print("b =", b)
+
+print("*****Boolean of number 0 as well as Boolean of empty string is False*****")
+
+print("bool(0): ", bool(0))
+print("bool(1): ", bool(1))
+print("bool(2): ", bool(2))
+
+print("")
+
+
+
+
