d0b033ed5d505ce2ea40202fd7d41d534b740702
ssp <swalehapeerzade@gmail.com>
2022-06-19 00:26:24 -0400

    commit

---
+++ loops.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "orange","pears","cherries","grapes"]
+
+for fruit in fruits:
+    print("Would you like {} ?".format(fruit))
+
+
+for number in range(1, 11):
+    print("Number {}".format(number))
+
+
