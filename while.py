fc4196bbb4e287e47231a919402dbf40ee385bbd
ssp <swalehapeerzade@gmail.com>
2022-06-19 00:33:03 -0400

    commit

---
+++ while.py
@@ -0,0 +1,13 @@
+"""
+This program demonstrates the use of while loop
+
+"""
+
+temp_f = 40
+
+while temp_f > 32:
+    print("The water is {} degrees".format(temp_f))
+    temp_f -= 1
+
+
+print("Water becomes ice at 32 degrees fahrenheit")
