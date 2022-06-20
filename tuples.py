5783c4fd10e56c3199d141ae6c780694958cd5a7
ssp <swalehapeerzade@gmail.com>
2022-06-19 21:18:15 -0400

    commit

---
+++ tuples.py
@@ -0,0 +1,12 @@
+"""
+This program demonstrates the use of tuples
+Tuples are immutable. It means you cannot change it once it is created
+"""
+
+my_tuple = (1, 2, 3, 4, 4, 5)
+print(my_tuple)
+
+print(my_tuple[0:3]) 
+print(my_tuple.index(2))
+print(my_tuple.count(4))
+
