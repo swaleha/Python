8369bd7f0188a136a2b8aef9095f40fd9da08cd7
ssp <swalehapeerzade@gmail.com>
2022-06-28 17:53:13 -0400

    commit

--- exception_handling.py
+++ exception_handling.py
@@ -2,8 +2,8 @@
 Exception is an event that occurs during execution of a program and disrupts the flow
 """
 
-def exceptionHandling():
 
+def exceptionHandling():
     try:
         a = 10
         b = "str"
@@ -12,11 +12,15 @@ def exceptionHandling():
         d = (a + b) / c
         print(d)
 
-    except ZeroDivisionError:
-        print("Zero division")
 
-    except TypeError:
-        print("Can't add string to integer")
+    #except ZeroDivisionError:
+        #print("Zero division")
+
+    #except TypeError:
+       # print("Can't add string to integer")
+
+    except:
+        print("You are the in the except block")
 
 
 exceptionHandling()
