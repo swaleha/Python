5d7d7f8f286241da9be6c3444c46321941a7bcad
ssp <swalehapeerzade@gmail.com>
2022-07-20 19:05:13 -0400

    commit

--- Projects/bmi_calculator.py
+++ Projects/bmi_calculator.py
@@ -4,6 +4,7 @@ weight = float(input("enter your weight in kg: "))
 print(f"weight = {weight}")
 print(f"height = {height}")
 bmi = round(weight / height ** 2)
+
 if bmi < 18.5:
   print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
