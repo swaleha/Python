96cd4cc14dfed2b2a8b75f90fddd5b147589c56b
ssp <swalehapeerzade@gmail.com>
2022-07-20 14:49:02 -0400

    commit

---
+++ Basics/age_calculator.py
@@ -0,0 +1,27 @@
+###
+Age calculator program: considering the life expectancy of an individual is 90
+###
+
+age = input("What is your current age?")
+print(age)
+years_left = 90 - int(age)
+weeks = 52 * years_left
+months = 12 * years_left
+days = 365 * years_left
+print(f"You have {days} days, {weeks} weeks and {months} months")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basics/print_statement.py
@@ -0,0 +1,8 @@
+"""
+Band name generator program
+"""
+
+print("Welcome to the band name generator program!")
+street = input("What city did you grew up in? ")
+pet = input("What is the name of your pet? ")
+print("Your band name could be :"+street+" "+pet)
